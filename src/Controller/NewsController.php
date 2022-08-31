5b8c077e96d35405eafff998402f3c5928e926d9
Roman Glukhenko <grn-it@yandex.ru>
2022-08-31 12:47:26 +0400

    Added item action to NewsController

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -3,12 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\News;
-use App\Repository\NewsRepository;
 use App\Service\News\NewsPersister;
+use App\Service\News\NewsProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -16,6 +15,14 @@ class NewsController extends AbstractController
 {
     public function __construct(private readonly SerializerInterface $serializer) {}
 
+    #[Route('/news/{id}', 'news_item', methods: ['GET'])]
+    public function item(Request $request, int $id, NewsProvider $newsProvider): JsonResponse
+    {
+        return $this->json(
+            $newsProvider->provide($id)
+        );
+    }
+    
     #[Route('/news', 'news_add', methods: ['POST'])]
     public function add(Request $request, NewsPersister $newsPersister): JsonResponse
     {
